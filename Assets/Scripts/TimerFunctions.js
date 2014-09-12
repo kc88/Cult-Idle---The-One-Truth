86502048ac523ee8fbd940b40433012fefbd30a3
kc88 <kc-1988@hotmail.com>
2014-09-12 09:40:45 -0400

    Update TimerFunctions.js
    Commented in a few notes.  Adjusted the way Variables are declared.  You should use access modifiers to improve on OOP.  It can be annoying at first, but it is strongly recommended.  Line: 13.  Not sure if this is an issue in JS or not, but in AS you needs to set the values to that type of value or they are converted right away.  Very annoying; Not sure if it's an issue in JS though.

--- Assets/Scripts/TimerFunctions.js
+++ Assets/Scripts/TimerFunctions.js
@@ -5,12 +5,12 @@ private var needInvoking : boolean;
 private var currentConverters : int;
 private var currentConvertSpeed : float;
 
-var mCon : MechanicsControl;
-mCon = gameObject.GetComponent("MechanicsControl");
-var cGUI : CentralGUI;
-cGUI = gameObject.GetComponent("CentralGUI");
+// Decalre new variables with set values.  Shorten code, looks nice, Easier to change later on.
+// It's strongly recommended to set access modifier to variables, as JS is an OOP language. ( Private, Public, Protected)
+var mCon : MechanicsControl = gameObject.GetComponent("MechanicsControl");
+var cGUI : CentralGUI = gameObject.GetComponent("CentralGUI");
 
-var cashTimer : float = 1;
+var cashTimer : float = 1.0;	// Added .0 as this is a float.  Does it lose it's Data Type if .0 is not set in JS?  Not sure.
 var authorityTimer : float = 0.2;
 
 
@@ -19,9 +19,11 @@ function Start () {
 }
 
 function Update () {
-	if(mCon.numFollowers != currentConverters || mCon.numConvertPerSecond != currentConvertSpeed)
+	if(mCon.numFollowers != currentConverters || mCon.numConvertPerSecond != currentConvertSpeed){
 		CancelInvoke();
-		needInvoking = true;
+		needInvoking = true;	
+	}
+	// The next line can be called if numFollowers is 0.001, -15, ect.  Will the code prevent these values?
 	if(needInvoking && mCon.numFollowers != 0)
 	{
 		InvokeRepeating("PerformAttempt",1.0/(mCon.numFollowers * mCon.numConvertPerSecond * cGUI.convSliderNew/100),1.0/(mCon.numFollowers * mCon.numConvertPerSecond * cGUI.convSliderNew/100));
@@ -42,11 +44,11 @@ function Update () {
 	}
 
 	if (authorityTimer < 0)
-		{
-			authorityTimer = 0.2;
-			mCon.AddSuspicion(cGUI.convSliderNew,cGUI.disSliderNew); //sending percent converting, percent distracting
-			//this is where suspicion checks
-		}
+	{
+		authorityTimer = 0.2;
+		mCon.AddSuspicion(cGUI.convSliderNew,cGUI.disSliderNew); //sending percent converting, percent distracting
+		//this is where suspicion checks
+	}
 	
 }
 
